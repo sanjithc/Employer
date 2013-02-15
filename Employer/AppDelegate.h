688483cd08f66c3598320c40f357dee21ba153b9
Sanjit Janak Nair <sanjitn@Sanjits-Mac-mini.local>
2013-02-15 15:53:30 +0530

    Initial Commit

---
+++ Employer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Employer
+//
+//  Created by Sanjit Janak Nair on 15/02/13.
+//  Copyright (c) 2013 HireCraft Software Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Employer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Employer
+//
+//  Created by Sanjit Janak Nair on 15/02/13.
+//  Copyright (c) 2013 HireCraft Software Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
